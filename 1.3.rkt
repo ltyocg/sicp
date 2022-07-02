537b05295706d7942cdd647ab7355f569dba9b33
John Liu <ltyocg@163.com>
2022-07-02 21:57:59 +0800

    初始化

---
+++ 1.3.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(define (max x y z)
+  (cond ((and (>= y x) (>= z x)) (+ y z))
+        ((and (>= x y) (>= z y)) (+ x z))
+        (else (+ x y))))
---
+++ 1.6.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+;5
+(new-if (= 2 3) 0 5)
+;0
+(new-if (= 1 1) 0 5)
+(define (square x) (* x x))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
