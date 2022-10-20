97cc2e0adde7ea85dad6bca8d996d703541dc18d
tianyu.liu <tianyu.liu@qunar.com>
2022-10-20 21:17:27 +0800

    练习

--- 1/1.29.rkt
+++ 1/1.29.rkt
@@ -5,8 +5,15 @@
       (+ (term a)
          (sum term (next a) next b))))
 (define (integral f n a b)
-  (let ((h (/ (- b a) n)))
-    (/
-     (* h (sum f))
-     3)
-    ))
+  (define h (/ (- b a) n))
+  (define (double num) (* 2 num))
+  (define (next num) (+ num (double h)))
+  (*
+   (/ h 3)
+   (+
+    (f a)
+    (f b)
+    (* 4 (sum f (+ a h) next (- b h)))
+    (double (sum f (+ a (double h)) next (- b (double h)))))))
+(integral (lambda (x) (* x x)) 100 0 3)
+(integral (lambda (x) (* x x)) 1000 0 3)
---
+++ 1/1.31.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+(define (factorial n)
+  (product (lambda (x) x) 1 inc n))
+(define (pi scale)
+  (* 4 (product
+        (lambda (x)
+          (let ((n (inc (* 2 x))))
+            (/ (* (dec n) (inc n)) (* n n))))
+        1
+        inc
+        scale)))
