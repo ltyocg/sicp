b5e521054e1119c72f74279f8d35eb8eb7d11065
John Liu <ltyocg@163.com>
2022-10-27 00:00:08 +0800

    练习

--- 2/2.28.rkt
+++ 2/2.28.rkt
@@ -2,8 +2,8 @@
 (define (fringe x)
   (cond
     ((null? x) nil)
-    ((not (pair? x)) x)
+    ((not (pair? x)) (list x))
     (else
-     (append (list (fringe (car x)))
+     (append (fringe (car x))
              (fringe (cdr x))))))
 (fringe (let ((x '((1 2) (3 4)))) (list x x)))
